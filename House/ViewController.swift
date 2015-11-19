b9feaba7bd3e7a7c541dbc53f0fbff4db2a51cee
peterstudio <peterstudio@icloud.com>
2015-11-19 21:17:20 +0800

    add house

---
+++ House/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  House
+//
+//  Created by duwen on 15/11/19.
+//  Copyright © 2015年 peterstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
